3f964c93cbef4405737d11b84e977758c7c3166c
fusonk <fusonk@berea.edu>
2024-11-18 17:22:54 -0500

    Created game_run, monster, player and test_suite files.

---
+++ game_run.py
@@ -0,0 +1,34 @@
+######################################################################
+# Author: Kirsten Fuson
+# Username: fusonk
+#
+# Assignment: Final Project
+#
+# Purpose: This program is designed to test my final project
+#
+#
+######################################################################
+# Acknowledgements:
+#
+#
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+####################################################################################
+import pygame
+
+class Game:
+    def __init__(self):
+        """
+        Handles the basic logic needed to actually run the game
+        """
+
+
+def main():
+    """
+    Starts the dungeon crawler
+
+    :return: None
+    """
+
+if __name__ == "__main__":
+    main()
---
+++ test_suite.py
@@ -0,0 +1,33 @@
+######################################################################
+# Author: Kirsten Fuson
+# Username: fusonk
+#
+# Assignment: Final Project
+#
+# Purpose: This program is designed to test my final project
+#
+#
+######################################################################
+# Acknowledgements:
+#   Original test code: Scott Hegan
+#
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+####################################################################################
+
+from inspect import getframeinfo, stack
+
+def unittest(did_pass):
+    """
+    Prints the result of a test
+    :param did_pass: a boolean representing the test
+    :return: None
+    """
+
+    caller = getframeinfo(stack()[1][0])
+    linenum = caller.lineno
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)

e9f6ae785a18382802de4ddf805971d618f4e4c7
fusonk <fusonk@berea.edu>
2024-11-22 10:19:17 -0500

    Issue #2 in this file, added player to screen, created another branch to work through the issue

--- player.py
+++ player.py
@@ -15,25 +15,43 @@
 # Attribution-Noncommercial-Share Alike 3.0 United States License.
 ####################################################################################
 import pygame
-from player import *
-from dungeon import *
-from item import *
+# from dungeon import *
+# from item import *
 
 
-class player:
-    def __init__(self):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, screen_size):
         """
-        Handles the basic logic needed to use the character
-        """
-        pass
+        Represents the player in the game.
 
-    def move_direction(self):
+        :param screen_size: Screen size, for keeping character on the screen
         """
-        uses keys to determine how to move character
-        :param:
-        :return:
+        super().__init__()
+        self.screen_size = screen_size
+        print("Spawning player")
+        self.surf = pygame.image.load('image/player_pixel.png').convert_alpha()
+        self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+        self.rect = self.surf.get_rect()
+        self.rect.move_ip(self.screen_size[0]//2, self.screen_size[1]//2)
+        self.size = 5
+
+
+    def movement(self, keys):
         """
-        pass
+        Handles movement events from the player
+
+        :param keys: key presses from pygame event listener
+
+        :return: None
+        """
+        if keys[pygame.K_UP]:
+            self.rect.move_ip(0, -3)
+        elif keys[pygame.K_DOWN]:
+            self.rect.move_ip(0, 3)
+        if keys[pygame.K_RIGHT]:
+            self.rect.move_ip(3, 0)
+        elif keys[pygame.K_LEFT]:
+            self.rect.move_ip(-3, 0)
 
     def take_damage(self):
         """
